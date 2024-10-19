2e3b4fc72bfd591f4454ed8b1f12aabafc52df24
Prerak <96664052+Prerak8880@users.noreply.github.com>
2024-10-20 00:57:20 +0530

    Create lcd.c

---
+++ 16x4_LCD/lcd.c
@@ -0,0 +1,63 @@
+/*
+ * File:   clcd.c
+ * Author: prerak8880
+ *
+ * Created on 19 October, 2024, 5:16 AM
+ */
+
+
+#include <xc.h>
+#include "clcd.h"
+
+static void clcd_write(unsigned char byte, unsigned char mode){
+    CLCD_RS = (__bit)mode; 
+    CLCD_DATA_PORT = byte;
+    
+    CLCD_EN = HIGH;
+    __delay_us(100);
+    CLCD_EN = LOW;
+    __delay_us(4100);
+}
+
+static void init_display_controller(void){
+    
+    __delay_ms(30);
+    
+    clcd_write(EIGHT_BIT_MODE, INST_MODE);
+    __delay_us(4100);
+    
+    clcd_write(EIGHT_BIT_MODE, INST_MODE);
+    __delay_us(100);
+    
+    clcd_write(EIGHT_BIT_MODE, INST_MODE);
+    __delay_us(1);
+    
+    clcd_write(CLEAR_DISP_SCREEN, INST_MODE);
+    __delay_us(100);
+    
+    clcd_write(DISP_ON_AND_CURSOR_OFF, INST_MODE);
+    __delay_us(100);
+}
+
+void init_clcd(void){
+    CLCD_DATA_PORT_DDR = 0x00;
+    
+    CLCD_RS_DDR = 0;
+    CLCD_EN_DDR = 0;
+    
+    init_display_controller();
+}
+
+void clcd_putch(const char data, unsigned char addr){
+    clcd_write(addr,INST_MODE);
+    clcd_write(data,DATA_MODE);
+}
+
+void clcd_print(const char *str, unsigned char addr){
+    clcd_write(addr,INST_MODE);
+    
+    while(*str != '\0'){
+        clcd_write(*str,DATA_MODE);
+        str++;
+    }
+}
