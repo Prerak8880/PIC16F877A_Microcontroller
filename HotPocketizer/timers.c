f3907b4fd487f533e75439d2e738ebdb13a98338
Prerak <96664052+Prerak8880@users.noreply.github.com>
2024-11-15 01:40:32 +0530

    Create timers.c

---
+++ HotPocketizer/timers.c
@@ -0,0 +1,24 @@
+/*
+ * File:   timers.c
+ * Author: Prerak
+ *
+ * Created on 21 September, 2024, 10:04 PM
+ */
+
+#include <xc.h>
+
+void init_timer2(void)
+{
+    /* Selecting the scale as 1:16 */
+    T2CKPS0 = 1;
+    T2CKPS1 = 1;
+
+    /* Loading the Pre Load register with 250 */
+    PR2 = 250; 
+    
+    /* The timer interrupt is enabled */
+    TMR2IE = 1;
+       
+    /* Switching on the Timer2 */
+    TMR2ON = 1;
+}
